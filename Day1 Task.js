e82d6c7bb1288827b205557d324c78d32ef83a94
Asma Siddiq <129653010+asmaerror06@users.noreply.github.com>
2024-07-20 20:01:39 +0500

    Script 1

---
+++ Day1 Task.js
@@ -0,0 +1,26 @@
+
+//! Number
+let num = 10;
+console.log(num)   
+console.log(typeof num)
+
+//! String
+let str = "hello";
+console.log(str)
+console.log(typeof str) 
+
+//! Boolean
+let boo = true;
+console.log(boo) 
+console.log(typeof boo)
+
+
+//! Object 
+let obj1 = { name: "John", age: 30 };
+console.log(obj1) 
+console.log(typeof obj1)
+
+//! Array
+let arr1 = [1, 2, 3];
+console.log(arr1) 
+console.log(typeof arr1);
