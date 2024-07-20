bc1abf61861d49bb929476eafca4fdfc45dcff7a
Asma Siddiq <129653010+asmaerror06@users.noreply.github.com>
2024-07-20 20:02:05 +0500

    Script 2

---
+++ script2.js
@@ -0,0 +1,56 @@
+//  Activity 4,5
+// Task 5,6
+let name = "xyz"
+console.log(name);
+
+name = "adam"
+console.log(name);
+
+const num1 = 5
+console.log(num1);
+
+num = 9
+console.log(num); //! TypeError
+
+//! Task 1 
+var num2 = 123
+console.log(num2);
+
+//! Task 2
+let clg = "xyz"
+console.log(clg);
+
+//! Task 3
+const vl = true
+console.log(vl);
+
+//! Task 4
+let numb = 935;
+let prgrming = "Java"
+let value = true 
+let obj = {
+    Name: "Aryan",
+    age: 26,
+};
+
+let arr = [1,3,4,5];
+
+console.log(typeof(numb));
+console.log(typeof(prgrming));
+console.log(typeof(value));
+console.log(typeof(obj));
+console.log(typeof(arr));
+
+//! Task 5 
+let a = 5
+console.log(a);
+
+a = 6
+console.log(a);
+
+//! Task 6 
+const b = 8
+console.log(b);
+
+b = 6
+console.log(b);
