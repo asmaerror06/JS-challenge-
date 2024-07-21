09fb24fc4ea480ec27feff09c9b97606353ac3e3
Asma Siddiq <129653010+asmaerror06@users.noreply.github.com>
2024-07-21 22:32:10 +0500

    Day3-Script-Task.js

--- Activity1.js
+++ Activity1.js
@@ -1,11 +1,38 @@
-// Task 1-5
+// Task 1-2
 
 
-let a = 4;
-b = 3;
+// If-else statement
 
-console.log(a+b);
-console.log(a-b);
-console.log(a*b);
-console.log(a/b);
-console.log(a%b);
+const num = 4;
+if(num > 4) {
+    console.log("Number is Positive")
+} else if(num < 4) {
+    console.log("Number is Negative")
+}else{
+    console.log("Number is Zero")
+        }
+
+
+    // Task 2
+
+    const age = 18;
+    if(age > 18) {
+        console.log("Eligible for vote");
+    }else {
+        console.log("Not Eligible for vote");
+    }
+
+
+    // Task 3
+
+let nest = 2;
+let nest1 = 4;
+let nest2 = 7;
+
+if(nest1>nest){
+    console.log("Nested number is greater");
+} else if(nest2 > nest1){
+    console.log("Nested number is greater");
+}else {
+    console.log("Nested number is greater");
+}
